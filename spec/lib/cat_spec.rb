7c9181fa9714f6cb9c37776f8a2a238a949cdcff
Benjamin <ashanin.benjamin@gmail.com>
2022-02-22 09:15:20 +0300

    Changes

--- spec/lib/cat_spec.rb
+++ spec/lib/cat_spec.rb
@@ -6,7 +6,7 @@ require 'cat'
 describe Cat do
   subject(:instance) { Cat.new(age: age, color: color) }
   let(:age) { 2 }
-  let(:color) { 'black' }
+  let(:color) { 'black1' }
 
   describe '#age' do
     subject { instance.age }
@@ -15,6 +15,6 @@ describe Cat do
 
   describe '#color' do
     subject { instance.color }
-    it { is_expected.to eq 'black' }
+    it { is_expected.to eq 'black1' }
   end
 end
